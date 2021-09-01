bfbba46ed716058317e6379a8c5dde04bd505135
uni2237 <yunhui.cho98@gmail.com>
2021-09-01 19:15:39 +0900

    수업의 정상

--- main.js
+++ main.js
@@ -2,98 +2,60 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
+function templateHTML(title,list,body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateList(filelist){
+  var list ='<ul>';
+  var i =0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i=i+1;
+  }
+  list=list+'</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
 
-  console.log(url.parse(_url,true))
-
-  /* console.log(url.parse(_url,true))
-  1. http://localhost:3000/?id=HTML
-  Url {
-  protocol: null,
-  slashes: null,
-  auth: null,
-  host: null,
-  port: null,
-  hostname: null,
-  hash: null,
-  search: '?id=HTML',
-  query: [Object: null prototype] { id: 'HTML' },
-  pathname: '/',
-  path: '/?id=HTML',
-  href: '/?id=HTML'
-
-  2. http://localhost:3000/favicon.ico -> not found
-  Url {
-  protocol: null,
-  slashes: null,
-  auth: null,
-  host: null,
-  port: null,
-  hostname: null,
-  hash: null,
-  search: null,
-  query: [Object: null prototype] {},
-  pathname: '/favicon.ico',
-  path: '/favicon.ico',
-  href: '/favicon.ico'
-}
-}
-  */
   if(pathname === '/'){
     if (queryData.id === undefined){
+        fs.readdir('./data',function(error,filelist){ //data 폴더 속 파일 리스트들
 
+          var title = 'Welcome';
+          var description='Hello, Node.js';
+          var list=templateList(filelist);
+          var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
+          response.writeHead(200);
+          response.end(template);})
 
-        var title = 'Welcome';
-        var description='Hello, Node.js';
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      
     } else {
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-        var title = queryData.id;
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
+      fs.readdir('./data',function(error,filelist){
+
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list=templateList(filelist);
+          var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
+          response.writeHead(200);
+          response.end(template);
+          });
         });
       }
   } else {
